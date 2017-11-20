e21d39845d49cf50a3e3979b75cdb1673466224e
Ladany <ladanymt@student.cs.jmu.edu>
2017-11-20 15:37:27 -0500

    Now generating the master keys

---
+++ genKey.c
@@ -0,0 +1,43 @@
+/*
+Generate master keys and save to binary files
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* OpenSSL headers */
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+void main()
+{
+    uint8_t key[EVP_MAX_KEY_LENGTH] ;
+    unsigned key_len = EVP_MAX_KEY_LENGTH ;
+    int fd_key ;
+
+    fd_key = open("amal_master_key.bin", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR) ;
+    if( fd_key == -1 )
+    {
+        fprintf(stderr, "Unable to open file for key\n");
+        exit(-1) ;
+    }
+
+    // Genrate the random amal master key
+    RAND_bytes( key , key_len );
+    write( fd_key , key , key_len );
+
+    fd_key = open("basim_master_key.bin", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR) ;
+    if( fd_key == -1 )
+    {
+        fprintf(stderr, "Unable to open file for key\n");
+        exit(-1) ;
+    }
+
+    // Genrate the random basim master key
+    RAND_bytes( key , key_len );
+    write( fd_key, key, key_len );
+
+    close( fd_key ) ;
+}
--- pa-final.sh
+++ pa-final.sh
@@ -8,10 +8,24 @@ rm -f dispatcher amal/amal amal/logAmal.txt basim/basim basim/logBasim.txt basim
 
 echo "=============================="
 echo "Compiling all source"
+    gcc genKey.c -o genKey -lcrypto
 	gcc amal/amal.c    myCrypto.c   -o amal/amal    -lcrypto
 	gcc basim/basim.c  myCrypto.c   -o basim/basim  -lcrypto
     gcc kdc/kdc.c      myCrypto.c   -o kdc/kdc      -lcrypto
 	gcc wrappers.c     dispatcher.c -o dispatcher
+
+echo
+echo "=============================="
+echo "Generating Master Keys"
+    ./genKey
+
+echo
+echo "Amal's master key:"
+hexdump -C amal_master_key.bin
+echo "Basim's master key:"
+hexdump -C basim_master_key.bin
+echo
+echo
 
 echo "=============================="
 echo "Starting the dispatcher"
